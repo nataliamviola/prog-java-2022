54867007b52b815aee07965938a8555f9886bd63
Natalia Viola <nataliamviola@gmail.com>
2022-05-11 22:21:49 -0300

    aula 16

---
+++ ProgJava2022/src/br/com/senaisp/aula16/classes/ParametroInvalidoException.java
@@ -0,0 +1,14 @@
+package br.com.senaisp.aula16.classes;
+
+public class ParametroInvalidoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	//Constructor
+	public ParametroInvalidoException(String arg0) {
+		super(arg0);
+	}
+
+}
