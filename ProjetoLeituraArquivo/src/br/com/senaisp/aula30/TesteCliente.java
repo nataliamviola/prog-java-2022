04455bf2643e86fe8baac011b503ca822fa3ca61
Joao Cezarotti <jlcezarotti@gmail.com>
2022-06-20 21:08:06 -0300

    complemento aula 30

--- ProjetoLeituraArquivo/src/br/com/senaisp/aula30/TesteCliente.java
+++ ProjetoLeituraArquivo/src/br/com/senaisp/aula30/TesteCliente.java
@@ -8,9 +8,12 @@ public class TesteCliente {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Cliente cli = new Cliente();
-		cli.importarArquivo("C:\\Users\\Aluno\\Downloads\\convertcsv.csv", TipoArquivo.CSV);
+		//cli.importarArquivo("C:\\Users\\Aluno\\Downloads\\convertcsv.csv", TipoArquivo.CSV);
+		//cli.importarArquivo("C:\\Users\\Aluno\\Downloads\\data.JSON", TipoArquivo.JSON);
+		
+		cli.importarArquivo("C:\\Users\\Aluno\\Downloads\\convertjson.xml", TipoArquivo.XML);
 		for (Object obj []: cli.getLstClientes()) {
-			System.out.println(obj [0]);
+			System.out.println(obj [0] + " - " + obj [1] + " - " + obj [2] + " - " + obj [3] + " - " + obj [4]);
 			
 		}
 
--- ProjetoLeituraArquivo/src/br/com/senaisp/aula30/classes/Cliente.java
+++ ProjetoLeituraArquivo/src/br/com/senaisp/aula30/classes/Cliente.java
@@ -14,12 +14,23 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 public class Cliente {
 	private String nome;
 	private int idade;
 	private String cpf;
 	private String rg;
 	private Date dtNascimento;
+	private String dtNasci;
 	
 	private List <Object[]> lstClientes;
 	private DateFormat dtFmt;
@@ -110,27 +121,111 @@ public class Cliente {
 	}
 	// criado os metodos 
 	private boolean lerXML(String strArquivo) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean ret = false;
+		lstClientes.clear();
+		try {
+			FileInputStream fis = new FileInputStream(strArquivo);
+			DocumentBuilderFactory fact = DocumentBuilderFactory.newInstance(); //criando o arquivo xml
+			DocumentBuilder build = fact.newDocumentBuilder(); //criando o xml e preparando pra leitura
+			Document doc = build.parse (fis); //pegando o doc xml e transformando em objeto (DOM-padrao de leitura xml)
+			//obtendo o elemento raiz
+			Element noRoot = doc.getDocumentElement();
+			//obtendo os elementos row 
+			NodeList listaNos = noRoot.getElementsByTagName("row");
+			//Percorrendo os elementos
+			for (int intI = 0; intI<listaNos.getLength(); intI++) {
+				Node item = listaNos.item(intI);
+				//verificando se é elemento xml
+				if (item instanceof Element) {
+					Element itt = (Element) item; //forçar um cast para ser elemento
+					nome = itt.getElementsByTagName("nome").item(0).getTextContent();
+					idade = Integer.parseInt(itt.getElementsByTagName("idade").item(0).getTextContent());
+					cpf = itt.getElementsByTagName("cpf").item(0).getTextContent();
+					rg = itt.getElementsByTagName("rg").item(0).getTextContent();
+					//dtNascimento = dtFmt.parse(itt.getElementsByTagName("data_nasc").item (0).getTextContent());
+					dtNasci = itt.getElementsByTagName("data_nasc").item(0).getTextContent();
+					
+					Object obj[] = {
+							nome,
+							idade,
+							cpf,
+							rg,
+							//dtNascimento
+							dtNasci
+					};
+					lstClientes.add(obj);
+				}
+				
+			}
+			ret = true;
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		
+		return ret;
 	}
+	
+	
 	private boolean lerJSON(String strArquivo) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	private boolean lerCSV(String strArquivo) {
+		boolean ret = false;
 		lstClientes.clear();
 		try {
 			FileInputStream fis = new FileInputStream(strArquivo);
 			InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
 			BufferedReader buf = new BufferedReader(isr);
-			String linha = buf.readLine();
+			StringBuilder stb = new StringBuilder();
+			String linha;
+			
+			while ((linha = buf.readLine ()) != null){
+				stb.append (linha);
+				
+								
+			}
+			buf.close(); //fecha o buffer de leitura do arquivo
+			JSONArray itens = new JSONArray (stb.toString()); //criando o objeto veotr JSON a partir do texto JSON
+			for (int intI=0; intI<itens.length(); intI++) { //laço de repetição para ele ler os itens do arquivo, pois no original esta separado por { e se caracteriza como objeto Json
+				JSONObject it = itens.getJSONObject(intI);
+				Object obj [] = {
+					it.getString("nome"), //pegar dentro do objeto a string cpm propriedade nome e etc
+					it.getInt("idade"), //int porque no original esta sem aspas e como numero
+					it.getString("cpf"),
+					it.getString("rg"),
+					it.getString("data_nasc") //use como texto ou como o de baixo
+					//dtFmt.parse(it.getString("data_nasc"))
+				};
+				lstClientes.add(obj);
+			}
+			
+			ret = true; //se ele percorrer e não achar erro,
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ret;
+	}
+	
+	
+	
+	private boolean lerCSV(String strArquivo) {
+		boolean ret = false;
+		lstClientes.clear();
+		try {
+			FileInputStream fis = new FileInputStream(strArquivo);
+			InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+			BufferedReader buf = new BufferedReader(isr);
+			String linha = buf.readLine(); //nao ler a primeira linha que e o cabeçalho
 			// lendo a partir da segunda linha
 			while ((linha = buf.readLine ()) != null){
 				// fazendo a quebra do arquivo pra obter os valores
-				List <String> itens = Arrays.asList(linha.split("\\s*,\\s*")); //) significa qualquer texto entre virgulas
+				List <String> itens = Arrays.asList(linha.split("\\s*,\\s*")); //expressão regular que significa qualquer texto entre virgulas ou algum outro sinal (indicar o sinal adequado)
 				dtNascimento = dtFmt.parse(itens.get(4));
 				
-				Object obj []= {
+				Object obj []= { //nome, idade, cpf, rg, data nasc, sexo, signo
 					itens.get(0), //nome
 					Integer.parseInt(itens.get(1)), //idade
 					itens.get(2), //cpf
@@ -141,13 +236,14 @@ public class Cliente {
 				lstClientes.add(obj);
 				
 			}
-			buf.close();
+			buf.close(); //fecha o buffer de leitura do arquivo
+			ret = true; //se ele percorrer e não achar erro,
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return false;
+		return ret;
 	}
 	
 }
